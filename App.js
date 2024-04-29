3f329f9ba65851392e0174a4ba58695775fdd3d7
Nabeel Mumtaz <153012098+nabeelmumtaz92@users.noreply.github.com>
2024-04-29 15:14:49 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,121 @@
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
+import Quotes from './Quotes'
+import QuoteForm from './QuoteForm'
+
+// 👇 these are the types of actions that can change state
+const CREATE_QUOTE = 'CREATE_QUOTE'
+const DELETE_QUOTE = 'DELETE_QUOTE'
+const EDIT_QUOTE_AUTHENTICITY = 'EDIT_QUOTE_AUTHENTICITY' // 👈 toggles the apocryphal property of a single quote
+const SET_HIGHLIGHTED_QUOTE = 'SET_HIGHLIGHTED_QUOTE'     // 👈 highlights a quote (or un-highlights it)
+const TOGGLE_VISIBILITY = 'TOGGLE_VISIBILITY'             // 👈 toggles whether to show all or only non-apocryphal
+
+let id = 1
+const getNextId = () => id++ // 👈 this is a helper to create new quotes
+
+const initialState = {
+  displayAllQuotes: true,
+  highlightedQuote:null, 
+  quotes: [
+    {
+      id: getNextId(),
+      quoteText: "Don't cry because it's over, smile because it happened.",
+      authorName: "Dr. Seuss",
+      apocryphal: false, 
+    },
+    {
+      id: getNextId(),
+      quoteText: "So many books, so little time.",
+      authorName: "Frank Zappa",
+      apocryphal: false,
+    },
+    {
+      id: getNextId(),
+      quoteText: "Be yourself; everyone else is already taken.",
+      authorName: "Oscar Wilde",
+      apocryphal: false,
+    },
+  ]
+}
+// 👇 create your initial state object here
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case CREATE_QUOTE:
+        return {
+          ...state,
+          quotes: [...state.quotes, action.payload]
+        }
+      case DELETE_QUOTE:
+        return {
+          ...state,
+          quotes:state.quotes.filter(qt => qt.id !== action.payload)
+        }
+      case EDIT_QUOTE_AUTHENTICITY:
+        return {...state,
+        quotes:state.quotes.map (qt =>{
+          if (qt.id != action.payload) return qt
+          return {...qt, apocryphal:!qt.apocryphal}}
+        }
+
+      case TOGGLE_VISIBILITY:
+        return {...state }
+      default:
+        return state
+    }
+  // 👇 implement your reducer here using the action types above
+}
+
+export default function  TodoForm({ createQuote}){
+  // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer(reducer, initialState)
+    // 👇 create your reducer function here
+    const createQuote = ({ authorName, quoteText} ) => {
+      const newQuote = {id : getNextId(), authorName, quoteText, apocryphal: false }
+      dispatch({ type: CREATE_QUOTE, payload: newQuote })
+    }
+
+  const createQuote = ({ authorName, quoteText }) => {
+    const createQuote = ({ authorName, quoteText }) => {
+      const newQuote = { id: getNextId(), authorName, quoteText, apocryphal: false }
+      dispatch({ type: CREATE_QUOTE, payload: newQuote })
+    }
+    // 👇 use the helper function above to create a new quote
+    // 👇 and dispatch it over to the reducer
+  }
+  const deleteQuote = id => {
+    // 👇 implement
+    dispatch({ type:DELETE_QUOTE, payload:id })
+  }
+
+  const editQuoteAuthenticity = id => {
+    dispatch ({ type: EDIT_QUOTE_AUTHENTICITY, payload: id })
+    // 👇 implement
+  }
+  const setHighlightedQuote = id => {
+    dispatch ({type: SET_HIGHLIGHTED_QUOTE, payload: id })
+    highlightedQuote: state.highlightedQuote === action.payload ? null : action.payload
+    // 👇 implement
+  }
+  const toggleVisibility = () => {
+    // 👇 implement
+    dispatch({ type: TOGGLE_VISIBILITY })
+  }
+
+  return (
+    <div id="mp">
+      <h2>Module Project</h2>
+      <Quotes
+        quotes={state.quotes}
+        highlightedQuote = {state.highlightedQuote}
+        displayAllQuotes = {state.displayAllQuotes}
+        editQuoteAuthenticity = {editQuoteAuthenticity}
+        setHighlightedQuote = {setHighlightedQuote}
+        deleteQuote ={deleteQuote}
+        />
+      // 👇 lots of props are missing! Check the Quotes component
+      <QuoteForm
+        createQuote={createQuote}
+      />
+    </div>
+  )
+}
---
+++ QuoteForm.js
@@ -0,0 +1,78 @@
+import React, {useReducer } from 'react' // 👈 you'll need the reducer hook
+
+// 👇 these are the types of actions that can change state
+const CHANGE_INPUT = 'CHANGE_INPUT'
+const RESET_FORM = 'RESET_FORM'
+
+// 👇 create your initial state object here
+const initialState = {
+  authorName: '',
+  quoteText: '',
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload 
+      return {...state, [name] : value} 
+    }
+
+
+    case RESET_FORM: {
+      return {...state}
+        }
+    default: {
+      return state
+    }
+  }
+}
+// 👇 create your reducer function here
+
+export default function TodoForm({ createQuote }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  // 👇 use the reducer hook to spin up state and dispatch
+
+  const onChange = ({ target: {name, value} }) => {
+    dispatch ({ type: CHANGE_INPUT, payload :{name, value}})
+    // 👇 implement
+  }
+  const resetForm = () => {
+    dispatch ({ type: RESET_FORM })
+    // 👇 implement
+  }
+  const onNewQuote = () => {
+    evt.preventDefault()
+    const {authorName, quoteText } = state
+    createQuote ({authorName, quoteText })
+    resetForm()
+  }
+
+  // 👇 some props are missing in the JSX below:
+  return (
+    <form id="quoteForm" onSubmit={onNewQuote}>
+      <h3>New Quote Form</h3>
+      <label><span>Author:</span>
+        <input
+          type='text'
+          value ={state.authorName}
+          name='authorName'
+          placeholder='type author name'
+          onChange={onChange}
+        />
+      </label>
+      <label><span>Quote text:</span>
+        <textarea
+          type='text'
+          name='quoteText'
+          placeholder='type quote'
+          onChange={onChange}
+        />
+      </label>
+      <label><span>Create quote:</span>
+        <button
+          role='submit'
+        >DO IT!</button>
+      </label>
+    </form>
+  )
+}
